db1508f171594e677163db603adc3918ddd7580d
AlixMenard <amenard@ec-m.fr>
2025-04-17 14:13:22 +0200

    Added method to retrieve teams

--- db_methods.py
+++ db_methods.py
@@ -10,3 +10,16 @@ def get_session() -> PooledMySQLConnection | MySQLConnectionAbstract:
         password="azbecbaboevav",
         database="pronosmodo"
     )
+
+# Team stats
+def get_teams(region = None):
+    mydb = get_session()
+    mycursor = mydb.cursor(dictionnary=True)
+    sql = "SELECT slug, name, region, power FROM teams"
+    if region is not None:
+        sql += " WHERE region = %s"
+        mycursor.execute(sql, (region,))
+    else:
+        mycursor.execute(sql)
+    teams = mycursor.fetchall()
+    return teams
